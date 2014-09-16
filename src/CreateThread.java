9b77c5ab921aba9559e6954bef45aa11b3ee0e72
waitingliu <294592364@qq.com>
2014-09-16 10:08:56 +0800

    add some comments

--- src/CreateThread.java
+++ src/CreateThread.java
@@ -1,4 +1,3 @@
-
 public class CreateThread {
 	//没有为线程编写运行代码(run方法)，start之后这个线程就结束了
 	public void justCreate(){
--- src/MyRunnable.java
+++ src/MyRunnable.java
@@ -2,14 +2,20 @@ import java.util.Date;
 
 
 public class MyRunnable implements Runnable{
-
+	public static int count = 0;
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		System.out.println("create a new thread");
+		for(int i =0; i< 100;i++){
+		count++;
+		System.out.println(Thread.currentThread().getName()+":count-"+count);
+		}
+		//System.out.println("create a new thread");
 	}
 	public static void main(String args[]){
-		Thread thread = new Thread(new MyRunnable());
-		thread.start();
+		Thread thread1 = new Thread(new MyRunnable());//必须将实现了Runnable接口的类对象附着在线程上，才会实现线程行为。单纯调用run方法不会产生任何线程能力
+		Thread thread2 = new Thread(new MyRunnable());
+		thread1.start();
+		thread2.start();
+		System.out.println(count);
 	}
 }
