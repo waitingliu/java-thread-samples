c019ec34a3a96e65c5f5255bebd32331572cce4a
waitingliu <294592364@qq.com>
2014-09-25 15:47:49 +0800

    add some comments

--- src/lock/PrintQueue.java
+++ src/lock/PrintQueue.java
@@ -5,17 +5,21 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class PrintQueue {
+	//ReentrantLock是Lock接口的实现类
+	//Lock允许多个读线程和一个写线程
+	//比Synchronized关键字性能更好，更灵活（控制的获取和释放不需要出现在同一个块结构中）
 	private final Lock queueLock = new ReentrantLock();
 	public void printJob(Object document){
 		try {
-			queueLock.lock();
+			queueLock.lock();//上锁
+			//模拟打印行为
 			System.out.println(Thread.currentThread().getName()+" print a document:"+document);
 			TimeUnit.SECONDS.sleep(2);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}finally{
-			queueLock.unlock();
+			queueLock.unlock();//解锁
 		}
 	}
 }
