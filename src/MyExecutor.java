50086d3a5ca07d766fc7bee6ae396619829fbc87
waitingliu <294592364@qq.com>
2014-09-29 11:59:22 +0800

    executor.invokeAll invokeAny

--- src/MyExecutor.java
+++ src/MyExecutor.java
@@ -10,6 +10,8 @@ public class MyExecutor {
 			//使用execute方法代替显式创建Thread对象。
 			exec.execute(new Fibonacci(10));
 		}
+		//result = exec.invokeAny(tasks);执行器收到一个任务列表，运行里面的所有任务，并返回第一个完成的任务的执行结果
+		//resultList = exec.invokeAll();等待所有任务完成，返回所有任务的执行结果
 		exec.shutdown();//关闭executorService是在所有任务都执行完之后。
 		//确保shutdown()之后不再有任何任务的创建和调用。
 		//否则会抛出：java.util.concurrent.RejectedExecutionException
